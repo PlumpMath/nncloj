31fcc989c968e864c09c76d37264e837a9d62854
Johann Bestowrous <johann.bestowrous@gmail.com>
2014-03-07 10:56:16 -0800

    ajax code that works in advanced compilation

--- src/cljs/menu/menu.cljs
+++ src/cljs/menu/menu.cljs
@@ -1,31 +1,44 @@
 (ns nn.menu
   (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [goog.events :as events]
-            [cljs.core.async :as async :refer [>! <! put! chan sliding-buffer]]
-            [om.core :as om :include-macros true]
-            [cljs.reader :as reader]
-            [om-sync.util :refer [tx-tag edn-xhr]]
-            [goog.dom :as gdom]
-            [goog.debug :as debug]
-            [clojure.browser.net :as net]
-            [clojure.browser.event :as gevent]
-            [om.dom :as dom :include-macros true]))
+  (:require
+   [cljs.core.async :as async :refer [>! <! put! chan sliding-buffer]]
+   [om.core :as om :include-macros true]
+   [cljs.reader :as reader]
+   [om-sync.util :refer [tx-tag edn-xhr]]
+   [goog.dom :as gdom]
+   [goog.debug :as debug]
+   [goog.events :as events]
+   [om.dom :as dom :include-macros true])
+  (:import [goog.net XhrIo]
+           goog.net.EventType
+           [goog.events EventType]))
+
 (enable-console-print!)
 
 (def app-state (atom {:e-films nil}))
 
 (defn e-films [ev]
-
+  (print "i ran, atleast")
   (map #(select-keys % [:id :title :description :thumbnail])  (js->clj
                                          (.. ev -target getResponseJson -data -items) :keywordize-keys true)))
 
-(let [api-url "http://gdata.youtube.com/feeds/api/users/neurosisnow/uploads?max-results=4&v=2&alt=jsonc"
-      ;; initialize ajax client
-      xhr (net/xhr-connection.)]
-  ;; register handlers
-  (gevent/listen xhr :error #(print "Error" %1))
-  (gevent/listen xhr :success #(reset! app-state {:e-films (into [] (e-films %))}))
-  (net/transmit xhr api-url "GET" {:q "jsonc"}))
+(def url "http://gdata.youtube.com/feeds/api/users/neurosisnow/uploads?max-results=4&v=2&alt=jsonc")
+(defn xhr [{:keys [method url on-complete on-error]}]
+  (let [xhr (XhrIo.)]
+    (events/listen xhr goog.net.EventType.SUCCESS
+      (fn [e]
+        (on-complete (into [] (map #(select-keys % [:id :title :description :thumbnail])
+                                (js->clj (.. xhr getResponseJson -data -items) :keywordize-keys true))))))
+    (events/listen xhr goog.net.EventType.ERROR
+
+      (fn [e]
+        (on-error {:error (.getResponseText xhr)})))
+    (. xhr
+      (send url method
+        ;;#js {"Content-Type" "application/edn" "Accept" "application/edn"}
+            ))))
+
+
 
 (defn on-hover
   ([owner chan img]
@@ -49,7 +62,6 @@
                   (dom/div #js {:onClick #(on-click owner root-chan id selected)
                                 :onMouseEnter #(on-hover owner root-chan (@thumbnail :hqDefault))
                                 :onMouseLeave #(on-hover owner root-chan)
-                                :onTap #(on-click owner root-chan id selected)
                                 :style (clj->js (if (= selected id) {:display "none"} {:padding "2px"}))
                                 :className (if hovered "title now-reading trans" "title trans")
                                 } title)
@@ -70,6 +82,7 @@
             })
     om/IWillMount
                 (will-mount [_]
+                            (xhr {:method "GET" :url url :on-complete #(om/transact! app :e-films (fn [_] %)) :on-error #(print %)})
                             (let [root-chan (om/get-state owner [:chans :root-chan])]
                               (go (while true
                                     (let [[key val] (<! root-chan)]
